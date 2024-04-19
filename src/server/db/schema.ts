b902b1c7d6e1bc3fd8e392619f6b0d29d0918864
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-20 03:00:57 +0800

    refactor: rework schema ordering finish

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -22,6 +22,7 @@ export const createTable = pgTableCreator((name) => `kama-makuri_${name}`);
 export const users = createTable("user", {
   id: varchar("id", { length: 255 }).notNull().primaryKey(),
   name: varchar("name", { length: 255 }),
+  pity: integer("pity").default(0),
   email: varchar("email", { length: 255 }).notNull(),
   emailVerified: timestamp("emailVerified", {
     mode: "date",
@@ -57,7 +58,7 @@ export const progressions = createTable(
   }),
 );
 
-export const userRelations = relations(users, ({ one }) => ({
+export const userRelations = relations(users, ({ one, many }) => ({
   accounts: one(accounts, {
     fields: [users.id],
     references: [accounts.userId],
@@ -70,6 +71,9 @@ export const userRelations = relations(users, ({ one }) => ({
     fields: [users.id],
     references: [progressions.userId],
   }),
+  cards: many(userCards),
+  decks: many(decks),
+  wishHistories: many(wishHistories),
 }));
 
 /* -------------------------------------------------------------------------- */
@@ -138,3 +142,323 @@ export const verificationTokens = createTable(
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   }),
 );
+
+/* -------------------------------------------------------------------------- */
+/*                                    CARD                                    */
+/* -------------------------------------------------------------------------- */
+
+export const elementTypeEnum = pgEnum("element_type", [
+  "fire",
+  "water",
+  "earth",
+  "wind",
+  "light",
+  "dark",
+]);
+
+// base standard un-owned cards
+export const cards = createTable(
+  "card",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    stock: integer("stock").default(0),
+    element: elementTypeEnum("element").notNull(),
+  },
+  (card) => ({
+    nameIdx: index("card_name_idx").on(card.name),
+  }),
+);
+
+export const cardBaseStats = createTable(
+  "cardBaseStats",
+  {
+    cardId: varchar("cardId", { length: 255 })
+      .notNull()
+      .references(() => cards.id),
+    hp: integer("hp").notNull(),
+    atk: integer("atk").notNull(),
+    def: integer("def").notNull(),
+    spd: integer("spd").notNull(),
+    mana: integer("mana").notNull(),
+  },
+  (cardBaseStats) => ({
+    cardIdIdx: index("cardBaseStats_cardId_idx").on(cardBaseStats.cardId),
+  }),
+);
+
+export const cardRelations = relations(cards, ({ one, many }) => ({
+  baseStats: one(cardBaseStats, {
+    fields: [cards.id],
+    references: [cardBaseStats.cardId],
+  }),
+  rarities: many(rarityCards),
+  userCards: many(userCards),
+  wishes: many(wishHistories),
+  wishHistories: many(wishHistories),
+}));
+
+/* -------------------------------------------------------------------------- */
+/*                                   RARITY                                   */
+/* -------------------------------------------------------------------------- */
+
+export const rarityTypeEnum = pgEnum("rarity_type", [
+  "D",
+  "C",
+  "B",
+  "A",
+  "S",
+  "SS",
+  "SSR",
+]);
+
+export const rarities = createTable("rarity", {
+  id: varchar("id", { length: 255 }).notNull().primaryKey(),
+  name: rarityTypeEnum("rarity_type").notNull(),
+  probability: integer("probability").notNull(),
+});
+
+// attach rarities to cards
+export const rarityCards = createTable(
+  "rarityCard",
+  {
+    rarityId: varchar("rarityId", { length: 255 })
+      .notNull()
+      .references(() => rarities.id),
+    cardId: varchar("cardId", { length: 255 })
+      .notNull()
+      .references(() => cards.id),
+  },
+  (rc) => ({
+    compoundKey: primaryKey({
+      columns: [rc.rarityId, rc.cardId],
+    }),
+    rarityIdIdx: index("rarity_card_rarityId_idx").on(rc.rarityId),
+    cardIdIdx: index("rarity_card_cardId_idx").on(rc.cardId),
+  }),
+);
+
+export const rarityCardRelations = relations(rarityCards, ({ one }) => ({
+  rarity: one(rarities, {
+    fields: [rarityCards.rarityId],
+    references: [rarities.id],
+  }),
+  card: one(cards, { fields: [rarityCards.cardId], references: [cards.id] }),
+}));
+
+/* -------------------------------------------------------------------------- */
+/*                                    DECK                                    */
+/* -------------------------------------------------------------------------- */
+
+export const decks = createTable(
+  "deck",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    userId: varchar("userId", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+  },
+  (deck) => ({
+    userIdIdx: index("deck_userId_idx").on(deck.userId),
+  }),
+);
+
+export const deckRelations = relations(decks, ({ one, many }) => ({
+  user: one(users, { fields: [decks.userId], references: [users.id] }),
+  cards: many(cardDecks),
+}));
+
+// attach cards to decks with position
+export const cardDecks = createTable(
+  "cardDeck",
+  {
+    userCardId: varchar("userCardId", { length: 255 })
+      .notNull()
+      .references(() => rarityUserCards.id),
+    deckId: varchar("deckId", { length: 255 })
+      .notNull()
+      .references(() => decks.id),
+    position: integer("position").notNull(),
+  },
+  (cd) => ({
+    compoundKey: primaryKey({
+      columns: [cd.userCardId, cd.deckId],
+    }),
+    userCardIdx: index("card_deck_userCardId_idx").on(cd.userCardId),
+    deckIdIdx: index("card_deck_deckId_idx").on(cd.deckId),
+  }),
+);
+
+export const cardDeckRelations = relations(cardDecks, ({ one }) => ({
+  userCard: one(rarityUserCards, {
+    fields: [cardDecks.userCardId],
+    references: [rarityUserCards.id],
+  }),
+  deck: one(decks, {
+    fields: [cardDecks.deckId],
+    references: [decks.id],
+  }),
+}));
+
+/* -------------------------------------------------------------------------- */
+/*                                  USER CARD                                 */
+/* -------------------------------------------------------------------------- */
+
+// owned cards collection
+export const userCards = createTable(
+  "userCard",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    userId: varchar("userId", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    cardId: varchar("cardId", { length: 255 })
+      .notNull()
+      .references(() => cards.id),
+  },
+  (uc) => ({
+    userIdIdx: index("user_card_userId_idx").on(uc.userId),
+    cardIdIdx: index("user_card_cardId_idx").on(uc.cardId),
+  }),
+);
+
+export const userCardRelations = relations(userCards, ({ one, many }) => ({
+  card: one(cards, { fields: [userCards.cardId], references: [cards.id] }),
+  user: one(users, { fields: [userCards.userId], references: [users.id] }),
+  rarities: many(rarityUserCards),
+}));
+
+// owned cards with rarity, stats, and progression
+export const rarityUserCards = createTable(
+  "rarityUserCard",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    quantity: integer("quantity").notNull(),
+    userCardId: varchar("userCardId", { length: 255 })
+      .notNull()
+      .references(() => userCards.id),
+    rarityId: varchar("rarityId", { length: 255 })
+      .notNull()
+      .references(() => rarities.id),
+  },
+  (ruc) => ({
+    userCardIdIdx: index("rarity_user_card_userCardId_idx").on(ruc.userCardId),
+    rarityIdIdx: index("rarity_user_card_rarityId_idx").on(ruc.rarityId),
+  }),
+);
+
+export const rarityUserCardRelations = relations(
+  rarityUserCards,
+  ({ one, many }) => ({
+    userCard: one(userCards, {
+      fields: [rarityUserCards.userCardId],
+      references: [userCards.id],
+    }),
+    rarity: one(rarities, {
+      fields: [rarityUserCards.rarityId],
+      references: [rarities.id],
+    }),
+    stats: one(userCardStats),
+    progression: one(userCardProgressions),
+    decks: many(cardDecks),
+  }),
+);
+
+export const userCardStats = createTable(
+  "userCardStats",
+  {
+    userCardId: varchar("userCardId", { length: 255 })
+      .notNull()
+      .references(() => userCards.id),
+    hp: integer("hp").notNull(),
+    atk: integer("atk").notNull(),
+    def: integer("def").notNull(),
+    spd: integer("spd").notNull(),
+    mana: integer("mana").notNull(),
+  },
+  (ucs) => ({
+    userCardIdIdx: index("user_card_stats_userCardId_idx").on(ucs.userCardId),
+  }),
+);
+
+export const userCardProgressions = createTable(
+  "userCardProgression",
+  {
+    userCardId: varchar("userCardId", { length: 255 })
+      .notNull()
+      .references(() => userCards.id),
+    level: integer("level").notNull(),
+    exp: integer("exp").notNull(),
+  },
+  (ucp) => ({
+    userCardIdIdx: index("user_card_progression_userCardId_idx").on(
+      ucp.userCardId,
+    ),
+  }),
+);
+
+/* -------------------------------------------------------------------------- */
+/*                                    WISH                                    */
+/* -------------------------------------------------------------------------- */
+
+export const wishes = createTable(
+  "wish",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    rarityWeight: json("rarity_weight").default({
+      D: 100,
+      C: 50,
+      B: 25,
+      A: 10,
+      S: 5,
+      SS: 2,
+      SSR: 1,
+    }),
+    cost: integer("cost").notNull(),
+  },
+  (wish) => ({
+    nameIdx: index("wish_name_idx").on(wish.name),
+  }),
+);
+
+export const wishRelations = relations(wishes, ({ many }) => ({
+  histories: many(wishHistories),
+  cards: many(cards),
+}));
+
+export const wishHistories = createTable(
+  "wish_history",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    rarity: rarityTypeEnum("rarity_type").notNull(),
+    isSoftPity: boolean("isSoftPity").default(false),
+    isHardPity: boolean("isHardPity").default(false),
+    cardId: varchar("cardId", { length: 255 }).notNull(),
+    userId: varchar("userId", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    wishId: varchar("wishId", { length: 255 })
+      .notNull()
+      .references(() => wishes.id),
+    createdAt: timestamp("createdAt", { mode: "date" }).default(
+      sql`CURRENT_TIMESTAMP`,
+    ),
+    updatedAt: timestamp("updatedAt", { mode: "date" }).default(
+      sql`CURRENT_TIMESTAMP`,
+    ),
+  },
+  (wh) => ({
+    userIdIdx: index("wish_history_userId_idx").on(wh.userId),
+    wishIdIdx: index("wish_history_wishId_idx").on(wh.wishId),
+  }),
+);
+
+export const wishHistoryRelations = relations(wishHistories, ({ one }) => ({
+  user: one(users, { fields: [wishHistories.userId], references: [users.id] }),
+  wish: one(wishes, {
+    fields: [wishHistories.wishId],
+    references: [wishes.id],
+  }),
+  card: one(cards, { fields: [wishHistories.cardId], references: [cards.id] }),
+}));
