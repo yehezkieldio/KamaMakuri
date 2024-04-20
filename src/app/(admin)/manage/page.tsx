b25d33bfbfe9f453f5cf3a5bdcc5dc378d77f36c
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-20 22:25:12 +0800

    refactor: better initial text on manage dashboard

--- src/app/(admin)/manage/page.tsx
+++ src/app/(admin)/manage/page.tsx
@@ -1,10 +1,9 @@
 export default async function Manage() {
     return (
-        <div>
-            <h1>Management</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nihil ea quaerat quod, ad explicabo atque
-                dolorum consectetur placeat voluptas saepe ullam nostrum vel.
+        <div className="flex justify-center pt-8 text-center">
+            <p className="max-w-2xl leading-7">
+                Welcome to the management panel. <br /> Here you can manage users, wishes, and cards. Please select an
+                option from the sidebar.
             </p>
         </div>
     );
