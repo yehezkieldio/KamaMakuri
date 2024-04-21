02dc7b6feb6a6890f5b455a17da3459201c677d8
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-22 01:52:14 +0800

    refactor: use background color instead of muted for title dashboard

--- src/app/(admin)/manage/_components/vertical-nav.tsx
+++ src/app/(admin)/manage/_components/vertical-nav.tsx
@@ -22,7 +22,7 @@ export function VerticalNav({ session, children }: VerticalNavProps) {
         <>
             <div className="hidden border-r bg-muted/40 md:block">
                 <div className="flex h-full max-h-screen flex-col gap-2">
-                    <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                    <div className="flex h-14 items-center border-b bg-background px-4 lg:h-[60px] lg:px-6">
                         <Link href="/" className="flex items-center gap-2 font-semibold">
                             <Icons.logo className="h-6 w-6" />
                             <span>KamaMakuri</span>
