9bc5dda275c77c32ce44aad9d733f71301846a30
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-21 15:51:35 +0800

    refactor: use the session role to check for access rather than using a seperate api call to get the role data

--- src/app/(admin)/manage/layout.tsx
+++ src/app/(admin)/manage/layout.tsx
@@ -2,17 +2,13 @@ import { Avatar } from "@/app/(admin)/manage/_components/avatar";
 import { Breadcrumbs } from "@/app/(admin)/manage/_components/breadcrumbs";
 import { SideNav } from "@/app/(admin)/manage/_components/side-nav";
 import { getServerAuthSession } from "@/server/auth";
-import { api } from "@/trpc/server";
 import { redirect } from "next/navigation";
 
 export default async function ManageLayout({ children }: { children: React.ReactNode }) {
     const session = await getServerAuthSession();
     if (!session?.user) return redirect("/api/auth/signin/discord");
 
-    const me = await api.user.me();
-    const role = me?.role;
-
-    if (role !== "admin") {
+    if (session?.user.role !== "admin") {
         return redirect("/");
     }
 
