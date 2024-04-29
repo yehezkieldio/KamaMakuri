3b79c58bdb91ec7776eee656f70484208d376b02
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-04-29 18:36:32 +0800

    refactor: remove id from breadcrumb on edit pages

--- src/app/(admin)/manage/_components/breadcrumbs.tsx
+++ src/app/(admin)/manage/_components/breadcrumbs.tsx
@@ -11,36 +11,49 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
+interface Breadcrumb {
+    label: string;
+    href: string;
+}
+
 export function Breadcrumbs() {
     const path = usePathname();
 
-    const buildBreadcrumbs = () => {
+    const buildBreadcrumbs = (path: string) => {
         const pathParts = path.split("/").filter((part) => part !== "");
+        let breadcrumbs: Breadcrumb[] = [];
 
-        return pathParts.map((part, index) => {
-            let label = part;
+        pathParts.forEach((part, index) => {
+            let label = part.replace(/-/g, " ").replace(/^\w/, (c) => c.toUpperCase());
             let href = `/${pathParts.slice(0, index + 1).join("/")}`;
+
             if (part === "manage") {
                 href = "/manage";
             } else if (part === "users") {
                 href = "/manage/users";
+            } else if (part === "edit") {
+                const idIndex = pathParts.indexOf("edit") + 1;
+                if (idIndex < pathParts.length) {
+                    href = `/${pathParts.slice(0, idIndex + 1).join("/")}`;
+                    label = "Edit";
+                }
             }
-            label = label
-                .replace(/-/g, " ")
-                .split(" ")
-                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                .join(" ");
-            return {
-                label,
-                href,
-            };
+
+            breadcrumbs.push({ label, href });
         });
+
+        let editIndex = breadcrumbs.findIndex((crumb) => crumb.label.toLowerCase() === "edit");
+        if (editIndex !== -1 && editIndex < breadcrumbs.length - 1) {
+            breadcrumbs.splice(editIndex + 1, 1);
+        }
+
+        return breadcrumbs;
     };
 
     return (
         <Breadcrumb className="hidden md:flex">
             <BreadcrumbList>
-                {buildBreadcrumbs().map((item, index) => (
+                {buildBreadcrumbs(path).map((item, index) => (
                     <React.Fragment key={item.label}>
                         {index > 0 && <BreadcrumbSeparator />}
                         <BreadcrumbItem>
